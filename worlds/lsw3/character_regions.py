28738098198a6668ae25c1d98c42126aadb609a3
GLaDOS_BACKEND_001 <whirling.newt@gmail.com>
2026-08-29 15:02:33 -0500

    Fixed a few bugs - stud wallet caps actually apply now - client should no longer boot if failing to connect to dme - Win con is no longer all 18 red bricks, just the randomized ones New - can now exclude a few annoying to get characters from the pool. Probably.
    Whoops forgot the apworld

---
+++ worlds/lsw3/character_regions.py
@@ -0,0 +1,88 @@
+# =========================================================
+# Character configuration
+# =========================================================
+hub_characters = {
+	"Cad Bane",
+	"Count Dooku",
+	"Admiral Yularen",
+	"Jango Fett",
+	"R4-P17",
+	"Neimoidian",
+	"Battle Droid",
+	"Super Battle Droid",
+	"Gonk Droid",
+	"LEP Servant Droid",
+	"Gold Super Battle Droid",
+	"Droideka",
+	"Captain Typho",
+	"Queen Neeyutnee",
+	"Battle Droid Commander",
+	"Hondo Ohnaka",
+	"Pirate Ruffian",
+	"Senator Kharrus",
+	"Tee Watt Kaa",
+	"Turk Falso",
+	"Probe Droid",
+	"Lurmen Villager",
+	"TX-20",
+	"Geonosian Guard",
+	"Workout Clone Trooper",
+	"Bib Fortuna",
+	"Undead Geonosian",
+	"Heavy Super Battle Droid",
+	"R6-H5",
+	"Clone Pilot",
+	"MSE-6",
+	"Sionver Boll",
+	"Bail Organa",
+	"Luxury Droid",
+	"Onaconda Farr",
+	"Senator Philo",
+	"Senate Commando (Captain)",
+	"Senate Commando",
+	"Gamorrean Guard",
+	"General Grievous",
+}
+
+minikit_characters = {
+	"Admiral Ackbar (Classic)",
+	"Captain Antilles (Classic)",
+	"Chewbacca (Classic)",
+	"Han Solo (Classic)",
+	"Lando Calrissian (Classic)",
+	"Princess Leia (Classic)",
+	"Luke Skywalker (Classic)",
+	"Obi-Wan Kenobi (Classic)",
+	"Qui-Gon Jinn (Classic)",
+	"Rebel Commando (Classic)",
+	"Wedge Antilles (Classic)",
+	"Boba Fett (Classic)",
+	"Greedo (Classic)",
+	"Darth Maul (Classic)",
+	"Darth Sidious (Classic)",
+	"Darth Vader (Classic)",
+	"Darth Vader Battle Damaged (Classic)",
+	"Vader’s Apprentice (Classic)",
+	"Imperial Guard (Classic)",
+	"Clone Shadow Trooper (Classic)",
+	"Stormtrooper (Classic)",
+	"Tusken Raider (Classic)",
+}
+
+brig_characters = {
+	"Dr. Nuvo Vindi",
+	"Wat Tambor",
+	"Lok Durd",
+	"Poggle the Lesser",
+	"Nute Gunray",
+	"Whorm Loathsom",
+}
+
+ground_battle_characters = {
+	"Chancellor Palpatine",
+	"Grand Moff Tarkin"
+}
+
+# NOTE:
+# "Destroyer Droid" is listed as "Droideka" instead
+# "Siover Boll" and "Onaconda Farr" use the spellings from what I saw in game
